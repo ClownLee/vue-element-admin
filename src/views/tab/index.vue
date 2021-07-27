ebe01ac5cccd448572a8af8acf7f8239495363ec
ele_11_ven <lwentao@hotmail.com>
2021-07-27 16:05:18 +0800

    vue-element-admin + tailwindcss + lodash + dayjs +decimal.js

---
+++ src/views/tab/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="tab-container">
+    <el-tag>mounted times ：{{ createdTimes }}</el-tag>
+    <el-alert :closable="false" style="width:200px;display:inline-block;vertical-align: middle;margin-left:30px;" title="Tab with keep-alive" type="success" />
+    <el-tabs v-model="activeName" style="margin-top:15px;" type="border-card">
+      <el-tab-pane v-for="item in tabMapOptions" :key="item.key" :label="item.label" :name="item.key">
+        <keep-alive>
+          <tab-pane v-if="activeName==item.key" :type="item.key" @create="showCreatedTimes" />
+        </keep-alive>
+      </el-tab-pane>
+    </el-tabs>
+    <div class="flex flex-wrap">
+      <div class="flex-1 bg-red-200">{{ _.find([{name: 'zs', age: '26'}, {name: 'ls', age: '22'}, {name: 'ww', age: '28'}], { name: 'ls' }) }}</div>
+      <div class="flex-1 bg-blue-200">{{ dayjs().format('YYYY-MM-DD HH:mm:ss') }}</div>
+      <div class="flex-1 bg-pink-200">{{ decimal(2.324).mul(2.559).toFixed(2, decimal.ROUND_HALF_UP) }}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+import TabPane from './components/TabPane'
+
+export default {
+  name: 'Tab',
+  components: { TabPane },
+  data() {
+    return {
+      tabMapOptions: [
+        { label: 'China', key: 'CN' },
+        { label: 'USA', key: 'US' },
+        { label: 'Japan', key: 'JP' },
+        { label: 'Eurozone', key: 'EU' }
+      ],
+      activeName: 'CN',
+      createdTimes: 0
+    }
+  },
+  watch: {
+    activeName(val) {
+      this.$router.push(`${this.$route.path}?tab=${val}`)
+    }
+  },
+  created() {
+    // init the default selected tab
+    const tab = this.$route.query.tab
+    if (tab) {
+      this.activeName = tab
+    }
+  },
+  methods: {
+    showCreatedTimes() {
+      this.createdTimes = this.createdTimes + 1
+    }
+  }
+}
+</script>
+
+<style scoped>
+  .tab-container {
+    margin: 30px;
+  }
+</style>
